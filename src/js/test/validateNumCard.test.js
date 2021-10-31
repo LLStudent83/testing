d61bb07e5354c3724cc77ff857ce0f8bcb03910e
Dmitry Prigulnov <prigulnov@mail.ru>
2021-10-31 13:23:01 +0500

    Разделил expect

--- src/js/test/validateNumCard.test.js
+++ src/js/test/validateNumCard.test.js
@@ -1,7 +1,11 @@
 import checkValidInput from '../testNumberCard';
 
-test('проверка валидации номера карты', () => {
+test('проверка валидации валидного номера карты', () => {
   checkValidInput('2221004624178068');
   expect(checkValidInput('2221004624178068')).toBeTruthy();
+});
+
+test('проверка валидации не валидного номера карты', () => {
+  checkValidInput('2221004624178068');
   expect(checkValidInput('222100462417806')).toBeFalsy();
 });
